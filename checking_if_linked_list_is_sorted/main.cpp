bb68abcc479afcb5e5ac57521add33c48e68e3e2
ubyhzargam <ubyhzargam@gmail.com>
2023-07-17 01:00:00 +0530

    Initial Commit

---
+++ checking_if_linked_list_is_sorted/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  checking_if_linked_list_is_sorted
+//
+//  Created by Uby H on 17/07/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
